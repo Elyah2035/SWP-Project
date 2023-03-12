c17314bc0a7db2d1377807948340b17ca772841f
Elyah2035 <elyahzach001@gmail.com>
2023-03-12 16:39:25 +0100

    trying out random stuff

--- Vererbung/Vererbung.java
+++ Vererbung/Vererbung.java
@@ -1,10 +1,13 @@
 package Vererbung;
-
+import java.util.Random;
 public class Vererbung {
     public static void main(String[] args) {
+            Random rand = new Random();
+        int randomNum = rand.nextInt(10); // Generate a random integer between 0 and 9
+        System.out.println("Random number: " + randomNum);
 
 
-        System.out.println("This is a test");
+
 
 
     }
